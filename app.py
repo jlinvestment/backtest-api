6d009dba96e6c639a9849b6b11d996797c89b218
jlinvestment <jlinvestmentgg@gmail.com>
2026-06-12 14:07:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-app = Flask(**name**)
-
-# CORS 허용
-
+app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def home():
-return "API RUNNING"
+    return "API RUNNING"
 
 @app.route("/backtest")
 def backtest():
 
-```
-symbol = request.args.get("symbol", "SPY")
-amount = float(request.args.get("amount", 300))
-years = int(request.args.get("years", 10))
+    symbol = request.args.get("symbol", "SPY")
+    amount = float(request.args.get("amount", 300))
+    years = int(request.args.get("years", 10))
 
-return jsonify({
-    "symbol": symbol,
-    "values": [100, 120, 140, 180],
-    "return": 18.5
-})
+    return jsonify({
+        "symbol": symbol,
+        "values": [100, 120, 140, 180],
+        "return": 18.5
+    })
 ```
 
 if **name** == "**main**":
